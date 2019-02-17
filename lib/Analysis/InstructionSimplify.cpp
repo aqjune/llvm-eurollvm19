aqjune/llvm-intptr

Add psub optimization

lib/Analysis/InstructionSimplify.cpp
@@ -4464,6 +4464,11 @@ static Value *SimplifyIntrinsic(Function *F, IterTy ArgBegin, IterTy ArgEnd,
         return SimplifyRelativeLoad(C0, C1, Q.DL);
       return nullptr;
     }
+    case Intrinsic::psub: {
+      if (Constant *Result = computePointerDifference(Q.DL, LHS, RHS))
+        return ConstantExpr::getIntegerCast(Result, F->getReturnType(), true);
+      return nullptr;
+    }
     default:
       return nullptr;
     }

aqjune/llvm-eurollvm19

lib/Analysis/InstructionSimplify.cpp
@@ -5078,6 +5078,10 @@ static Value *simplifyBinaryIntrinsic(Function *F, Value *Op0, Value *Op1,
     // TODO: maxnum(nnan ninf x, -flt_max) -> x
     break;
   }
+  case Intrinsic::psub:
+    if (Constant *Result = computePointerDifference(Q.DL, Op0, Op1))
+      return ConstantExpr::getIntegerCast(Result, F->getReturnType(), true);
+    break;
   default:
     break;
   }